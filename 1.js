e2bdd0a9655d5bf3d22d5ae051aeed7660b82f09
Hipidan <145728251+Hipidan@users.noreply.github.com>
2024-08-30 21:46:00 +0800

    更新 1.js

---
+++ 1.js
@@ -0,0 +1,14 @@
+{
+  "rules": [
+    {
+      "action": "block",
+      "conditions": [
+        {
+          "field": "secretaryVO.name",
+          "operator": "equals",
+          "value": "跟贴小秘书"
+        }
+      ]
+    }
+  ]
+}
